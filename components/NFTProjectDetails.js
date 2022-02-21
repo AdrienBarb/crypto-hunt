b1219d9e9585ceeb6067bb2c868d1fdb5d04bff1
Adrien Barbier <a.barbier@archeon-medical.com>
2022-02-21 15:59:47 +0100

    add nft projecr details

--- components/NFTProjectDetails.js
+++ components/NFTProjectDetails.js
@@ -10,6 +10,11 @@ import { useRouter } from "next/router";
 import { StyledNFTProjectDetails } from "../styles/StyledNFTProjectDetails";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { FiEdit2 } from "react-icons/fi";
+import { BsTwitter } from "react-icons/bs";
+import { AiFillHeart } from "react-icons/ai";
+import { CgWebsite } from "react-icons/cg";
+import { HiDocument } from "react-icons/hi";
+import { HorizontalMargin } from "../styles/StyledMargin";
 
 const NFTProjectDetails = ({ projectId, getCurrentNFTProject, state }) => {
   const router = useRouter();
@@ -43,6 +48,76 @@ const NFTProjectDetails = ({ projectId, getCurrentNFTProject, state }) => {
         </Link>
       </div>
       <HorizontalDivider color={Colors.yellow} width="100%" />
+      <StyledText karla>
+        {state.cryptoProjectsReducers.currentNFTProject?.description
+          ? state.cryptoProjectsReducers.currentNFTProject.description
+          : "-"}
+      </StyledText>
+
+      <HorizontalMargin m1 />
+
+      <div className="links-wrapper">
+        {state.cryptoProjectsReducers.currentNFTProject?.twitterLink && (
+          <>
+            <Link
+              href={state.cryptoProjectsReducers.currentNFTProject.twitterLink}
+            >
+              <a target="_blank">
+                <BsTwitter size={22} />
+              </a>
+            </Link>
+            <VerticalMargin m4 />
+          </>
+        )}
+        {state.cryptoProjectsReducers.currentNFTProject?.websiteLink && (
+          <>
+            <Link
+              href={state.cryptoProjectsReducers.currentNFTProject.websiteLink}
+            >
+              <a target="_blank">
+                <CgWebsite size={22} />
+              </a>
+            </Link>
+            <VerticalMargin m4 />
+          </>
+        )}
+        {state.cryptoProjectsReducers.currentNFTProject?.whitePaperLink && (
+          <Link
+            href={state.cryptoProjectsReducers.currentNFTProject.whitePaperLink}
+          >
+            <a target="_blank">
+              <HiDocument size={22} />
+            </a>
+          </Link>
+        )}
+      </div>
+      <HorizontalMargin m1 />
+
+      {state.cryptoProjectsReducers.currentNFTProject?.networkOwnerRewards &&
+        state.cryptoProjectsReducers.currentNFTProject?.addressOwnerRewards && (
+          <div className="bottom-card">
+            <div>
+              <StyledText>
+                Send tips to this adress, to thanks the people who discovered
+                the project:
+              </StyledText>
+              <StyledText>
+                Network:{" "}
+                {
+                  state.cryptoProjectsReducers.currentNFTProject
+                    .networkOwnerRewards
+                }
+              </StyledText>
+              <StyledText>
+                Adress:{" "}
+                {
+                  state.cryptoProjectsReducers.currentNFTProject
+                    .addressOwnerRewards
+                }
+              </StyledText>
+            </div>
+          </div>
+        )}
     </StyledNFTProjectDetails>
   );
 };
--- styles/StyledNFTProjectDetails.js
+++ styles/StyledNFTProjectDetails.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const StyledNFTProjectDetails = styled.div`
   .header {
@@ -6,4 +6,16 @@ export const StyledNFTProjectDetails = styled.div`
     justify-content: space-between;
     align-items: center;
   }
-`
+
+  .links-wrapper {
+    display: flex;
+  }
+
+  .bottom-card {
+    background-color: #dfe0e2;
+    zidth: 100%;
+    border-radius: 6px;
+    margin-top: 0.4rem;
+    padding: 0.4rem;
+  }
+`;
